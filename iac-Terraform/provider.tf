18a43c78e3e818e025a740c9f90e57423029d71e
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2025-10-22 12:58:00 +0100

    fixed aws profile issue with github terraform

--- iac-Terraform/provider.tf
+++ iac-Terraform/provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "juwonAdmin"
+  # shared_credentials_files = ["~/.aws/credentials"]
+  # profile                  = "juwonAdmin"
 }
